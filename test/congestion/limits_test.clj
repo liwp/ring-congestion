e82c70e43137db48143cd6d217bf4343034d2e7c
Lauri Pesonen <lauri.pesonen@iki.fi>
2014-10-24 11:45:56 +0100

    Add unit tests for ip limit

---
+++ test/congestion/limits_test.clj
@@ -0,0 +1,12 @@
+(ns congestion.limits-test
+  (:require [clj-time.core :as t]
+            [clojure.test :refer :all]
+            [congestion.limits :refer :all]))
+
+(deftest ^:unit test-ip-rate-limit
+  (testing "IpRateLimit"
+    (let [limit (->IpRateLimit (t/seconds 10) 100)
+          req {:remote-addr "127.0.0.1"}]
+      (is (= (get-quota limit) 100))
+      (is (= (get-key limit req) ":congestion.limits/ip-127.0.0.1"))
+      (is (= (get-period limit) (t/seconds 10))))))
