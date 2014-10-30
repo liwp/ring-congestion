43b2d419c28a92b1b98753d961e4bcce7ad6565b
Lauri Pesonen <lauri.pesonen@iki.fi>
2014-10-30 12:51:27 +0000

    Update the project file
    - EPL license
    - set up :scm and :deploy-repositories to make clojars work
    - remove deps: ring-core, friend, s3-wagon-private
    - remove private maven repo

--- project.clj
+++ project.clj
@@ -1,26 +1,23 @@
 (defproject listora/ring-congestion "0.1.0-SNAPSHOT"
   :description "Rate limiting ring middleware"
   :url "https://github.com/listora/ring-congestion"
-  :license {:distribution :manual}
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/listora/again"}
+  :deploy-repositories [["releases" :clojars]]
+
   :dependencies [[clj-time "0.8.0" :exclusions [org.clojure/clojure]]
                  [com.taoensso/carmine "2.7.1" :exclusions [org.clojure/clojure]]
                  [org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:dependencies [[ring/ring-core "1.3.1"]
-                                  [ring-mock "0.1.5"]
-                                  [com.cemerick/friend "0.2.1"]]
+
+  :profiles {:dev {:dependencies [[compojure "1.2.1"]
+                                  [ring-mock "0.1.5"]]
 
                    :plugins [[jonase/eastwood "0.1.4"]
-                             [listora/whitespace-linter "0.1.0"]
-                             [s3-wagon-private "1.1.2"]]
+                             [listora/whitespace-linter "0.1.0"]]
 
                    :eastwood {:exclude-linters [:deprecations :unused-ret-vals]}
 
                    :aliases {"ci" ["do" ["test"] ["lint"]]
-                             "lint" ["do" ["whitespace-linter"] ["eastwood"]]}}}
-
-  :repositories {"releases" {:url "s3p://eindx-maven/releases/"
-                             :username [:gpg :env/aws_access_key_id]
-                             :passphrase [:gpg :env/aws_secret_access_key]}
-                 "snapshots" {:url "s3p://eindx-maven/snapshots/"
-                              :username [:gpg :env/aws_access_key_id]
-                              :passphrase [:gpg :env/aws_secret_access_key]}})
+                             "lint" ["do" ["whitespace-linter"] ["eastwood"]]}}})
