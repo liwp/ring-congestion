cb202282e73c7be8f9aefe7971050f10d151f7f9
Lauri Pesonen <lauri.pesonen@iki.fi>
2014-11-05 15:48:38 +0000

    Use the eindx maven repo for now

--- project.clj
+++ project.clj
@@ -5,8 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url "https://github.com/listora/again"}
-  :deploy-repositories [["releases" :clojars]]
-
+  ;;:deploy-repositories [["releases" :clojars]]
+  :repositories {"releases" {:url "s3p://eindx-maven/releases/"
+                             :username [:gpg :env/aws_access_key_id]
+                             :passphrase [:gpg :env/aws_secret_access_key]}
+                 "snapshots" {:url "s3p://eindx-maven/snapshots/"
+                              :username [:gpg :env/aws_access_key_id]
+                              :passphrase [:gpg :env/aws_secret_access_key]}}
   :dependencies [[clj-time "0.8.0" :exclusions [org.clojure/clojure]]
                  [com.taoensso/carmine "2.7.1" :exclusions [org.clojure/clojure]]
                  [org.clojure/clojure "1.6.0"]]
@@ -14,8 +19,9 @@
   :profiles {:dev {:dependencies [[compojure "1.2.1"]
                                   [ring-mock "0.1.5"]]
 
-                   :plugins [[jonase/eastwood "0.1.4"]
-                             [listora/whitespace-linter "0.1.0"]]
+                   :plugins [[jonase/eastwood "0.1.5"]
+                             [listora/whitespace-linter "0.1.0"]
+                             [s3-wagon-private "1.1.2"]]
 
                    :eastwood {:exclude-linters [:deprecations :unused-ret-vals]}
 
